63fe69c8e5e6b084dadb6ad0d9f67463c9e7382c
javier <martinez.suarez270@gmail.com>
2024-03-13 10:00:33 -0500

    ajuste cambio de credenciales

--- proyecto/servicios/urls.py
+++ proyecto/servicios/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_list, name='home_list'),
+    path('sent_message', views.sent_message, name='sent_message'),
 ]
--- proyecto/servicios/views.py
+++ proyecto/servicios/views.py
@@ -7,6 +7,7 @@ from django_cron import CronJobBase, Schedule
 import pandas as pd
 from twilio.rest import Client
 from datetime import date, timedelta
+from django.http import HttpResponse
 #from django_crontab import daily
 
 # Create your views here.
@@ -35,19 +36,22 @@ def home_list(request):
             print(form.errors)
     return render(request, 'servicios/home_list.html',contex)
 
+
+
 def send_message(request):
+    print(request)
     fecha_actual = date.today()
     fecha_siguiente = fecha_actual + timedelta(days=1)
     account_sid = 'ACe57c7d027de7cc623d367042f681be03'
-    auth_token = 'c1c49653a2bc5cc02db62c8d88a4b862'
+    auth_token = '4ed73b4f420d6507765356c1ebd39ed2'
     body = f"Te avisamos que en el barrio {request.barrio.nombre} tendrá previstos cortes de agua el día {fecha_siguiente.strftime('%A, %d de %B de %Y')}, te recomendamos tener provisiones de agua en tu hogar 😊 🚱🤽‍♂️🚱"
     client = Client(account_sid, auth_token)
 
     message = client.messages \
                     .create(
                         body=body,
-                        from_='+17244924849',
-                        to='+570'+str(request.username)
+                        from_='+13017100811',
+                        to='+57'+str(request.username)
                     )
 
     print(message.sid)
@@ -55,14 +59,27 @@ def send_message(request):
 class Scrapping(CronJobBase):
     def do(self):
         account_sid = 'ACe57c7d027de7cc623d367042f681be03'
-        auth_token = 'c1c49653a2bc5cc02db62c8d88a4b862'
+        auth_token = '4ed73b4f420d6507765356c1ebd39ed2'
         client = Client(account_sid, auth_token)
         barrios_web_scrapping = generar_web_scrapping()
         df = pd.DataFrame(barrios_web_scrapping)
         df=df['Barrios'].str.split(', ')
         barrios_list=df.tolist()[0]
         barrios_list = [elemento.rstrip('.') for elemento in barrios_list]
-        
+        print(barrios_list)
         usuarios= UserProfile.objects.filter(barrio__nombre__in=barrios_list)
         for usuario in usuarios:
             send_message(usuario)
+
+def sent_message(request):
+    barrios_web_scrapping = generar_web_scrapping()
+    df = pd.DataFrame(barrios_web_scrapping)
+    df=df['Barrios'].str.split(', ')
+    barrios_list=df.tolist()[0]
+    barrios_list = [elemento.rstrip('.') for elemento in barrios_list]
+    
+    usuarios= UserProfile.objects.filter(barrio__nombre__in=barrios_list)
+    for usuario in usuarios:
+        send_message(usuario)
+    
+    return HttpResponse("Message sent successfully")
